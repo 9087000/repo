8c658a91b2629021dff9ab280cc972951f7c06a4
mukulvasants <mukulvasants@gmail.com>
2022-12-12 07:11:28 +0530

    full code

---
+++ src/test/java/com/example/icecontroller/IcecontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcecontrollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
